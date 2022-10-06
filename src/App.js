ca0d6b55d2eb89c4fada65e05507a4d259d767e1
KaleabIsDoneWithJava <kaleab.teshome16@gmail.com>
2022-10-06 09:26:16 +0300

    Writing more complex JSX code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import ExpenseItem from './component/ExpenseItem';
 
 function App() {
   return (
     <div className="App">
       <h1>Let's get started for sure sure!</h1>
+      <ExpenseItem></ExpenseItem>
     </div>
   );
 }
---
+++ src/component/ExpenseItem.js
@@ -0,0 +1,15 @@
+//import { startTransition } from "react";
+
+function ExpenseItem() {
+  return (
+    <div>
+        <div>October 10, 2022</div>
+        <div>
+            <h2>Car Insurance</h2>
+            <div>$456.78</div>
+        </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
