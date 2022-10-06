3d8cd5913ffbac8d28b9eb1acd7d68ebfa80c9d6
KaleabIsDoneWithJava <kaleab.teshome16@gmail.com>
2022-10-06 10:22:45 +0300

    Dynamic Data & Working with Expressions in JSX

--- src/component/ExpenseItem.js
+++ src/component/ExpenseItem.js
@@ -2,12 +2,15 @@
 import './ExpenseItem.css'
 
 function ExpenseItem() {
+    const expenseDate = new Date(2022, 10, 10);
+    const expenseTitle = 'Car Insurance';
+    const expenseAmount = 456.78;
   return (
     <div className='expense-item'>
-        <div>October 10, 2022</div>
+        <div>{expenseDate.toISOString()}</div>
         <div className='expense-item__description'>
-            <h2>Car Insurance</h2>
-            <div className='expense-item__price'>$456.78</div>
+            <h2>{expenseTitle}</h2>
+            <div className='expense-item__price'>${expenseAmount}</div>
         </div>
     </div>
   );
