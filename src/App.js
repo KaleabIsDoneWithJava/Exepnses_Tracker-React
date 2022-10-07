808a7b0f904ff81671ee0f71a8a4800885c4b898
KaleabIsDoneWithJava <kaleab.teshome16@gmail.com>
2022-10-07 10:47:34 +0300

    Splitting Components Into Multiple Components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 //import "./App.css";
-import ExpenseItem from "./component/ExpenseItem";
+//import ExpenseItem from "./component/ExpenseItem";
+import Expenses from "./component/Expenses";
 
 function App() {
   const expenses = [
@@ -32,30 +33,8 @@ function App() {
   return (
     <div className="App">
       <h1>Let's get started for sure sure!</h1>
+      <Expenses items = {expenses}/>
 
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      ></ExpenseItem>
-
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      ></ExpenseItem>
-
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      ></ExpenseItem>
-
-      <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      ></ExpenseItem>
     </div>
   );
 }
